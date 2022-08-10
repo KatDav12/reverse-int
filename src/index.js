a9b162909a3d022416563f60dc6036249f811eec
KatDav12 <fotonkat@gmail.com>
2022-08-10 22:07:09 +0500

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let m = Math.abs(n);
+  str = m.toFixed(0);
+  // let a = Math.abs(n / 100);  
+  // let b = Math.abs(((n % 100)/10));
+  // let c = Math.abs((n % 10));
+  return (str[2] + (str[1])+ (str[0]));
 }
